48375742002219c13fcfa47aeb9f751d1ae205bc
Hippolyte Durix <hdurix@ippon.fr>
2022-08-04 16:46:09 +0200

    Be sure unit tests can be run in parallel
    Fix #44

--- test/cli/fake-dir.fixture.ts
+++ test/cli/fake-dir.fixture.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as rimraf from 'rimraf';
 import * as fse from 'fs-extra';
 
-export const FAKE_DIR = path.resolve(__dirname, 'tmp');
+export const FAKE_DIR = path.resolve(__dirname, 'tmp', Math.random().toString());
 export const FAKE_SRC_DIR = path.resolve(FAKE_DIR, 'src');
 
 export const removeFakeDir = (): void => rimraf.sync(FAKE_DIR);
